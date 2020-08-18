205ef33716bcf58a6822695dcbbd85d79d4431df
Grzegorz Siewruk <gsiewruk+gitlab@gmail.com>
2020-08-18 13:16:32 +0200

    init commit

---
+++ src/main/java/io/mixeway/scanner/ScannerApplication.java
@@ -0,0 +1,13 @@
+package io.mixeway.scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/mixeway/scanner/ScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mixeway.scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
