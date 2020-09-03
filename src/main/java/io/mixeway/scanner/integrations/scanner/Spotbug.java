6e1c55932d18ec6f59392e804ece972afdb3b05b
Grzegorz Siewruk <gsiewruk+gitlab@gmail.com>
2020-09-03 18:31:21 +0200

    Fix For complex maven projects

--- src/main/java/io/mixeway/scanner/integrations/scanner/Spotbug.java
+++ src/main/java/io/mixeway/scanner/integrations/scanner/Spotbug.java
@@ -23,9 +23,14 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class Spotbug implements ScannerIntegrationFactory {
@@ -42,6 +47,8 @@ public class Spotbug implements ScannerIntegrationFactory {
     @Override
     public List<Vulnerability> runScan(ScanRequest scanRequest) throws IOException, InterruptedException {
         log.info("[Spotbug] Starting to package app {}", scanRequest.getTarget());
+        List<SpotbugReportXML> spotbugReportXMLS = new ArrayList<>();
+        List<Path> reportPaths = new ArrayList<>();
         String projectDirectory = CodeHelper.getProjectPath(scanRequest, false);
         ProcessBuilder packageApp = new ProcessBuilder("bash", "-c", "mvn package -DskipTests");
         packageApp.directory(new File(projectDirectory));
@@ -55,19 +62,34 @@ public class Spotbug implements ScannerIntegrationFactory {
         log.info("[Spotbug] Report ready to process {}", scanRequest.getTarget());
         SpotbugReportXML spotbugReportXML = processXmlReport(projectDirectory + File.separatorChar + "target" + File.separatorChar + "spotbugsXml.xml");
         log.info("[Spotbug] Scan completed");
-        return convertSpotbugReportIntoVulnList(spotbugReportXML);
+        searchForReports(spotbugReportXMLS, projectDirectory);
+        return convertSpotbugReportIntoVulnList(spotbugReportXMLS);
+    }
 
+    private void searchForReports(List<SpotbugReportXML> spotbugReportXMLS, String projectDirectory) throws IOException {
+        List<Path> reportPaths;
+        try (Stream<Path> paths = Files.walk(Paths.get(projectDirectory))) {
+            reportPaths = paths
+                    .filter(Files::isRegularFile)
+                    .filter(f -> f.getFileName().toString().equals("spotbugsXml.xml"))
+                    .collect(Collectors.toList());
+        }
+        for (Path path : reportPaths){
+            spotbugReportXMLS.add(processXmlReport(path.toString()));
+        }
     }
 
     /**
      * Converts spotbug XML report into Vulnerabilities
-     * @param spotbugReportXML report to parse
+     * @param spotbugReportXMLs report to parse
      * @return list of shared items
      */
-    private List<Vulnerability> convertSpotbugReportIntoVulnList(SpotbugReportXML spotbugReportXML) {
+    private List<Vulnerability> convertSpotbugReportIntoVulnList(List<SpotbugReportXML> spotbugReportXMLs) {
         List<Vulnerability> vulnerabilities = new ArrayList<>();
-        for (BugInstance bugInstance : spotbugReportXML.getBugInstanceList()){
-            vulnerabilities.add(new Vulnerability(bugInstance));
+        for (SpotbugReportXML reportXML : spotbugReportXMLs) {
+            for (BugInstance bugInstance : reportXML.getBugInstanceList()) {
+                vulnerabilities.add(new Vulnerability(bugInstance));
+            }
         }
         return vulnerabilities;
     }
@@ -75,25 +97,21 @@ public class Spotbug implements ScannerIntegrationFactory {
     @Override
     public List<Vulnerability> runScanStandalone() throws IOException, InterruptedException {
         try {
+            List<SpotbugReportXML> spotbugReportXMLS = new ArrayList<>();
+            List<Path> reportPaths = new ArrayList<>();
             String projectDirectory = CodeHelper.getProjectPath(new ScanRequest(), true);
-            log.info("[Spotbug] Starting to package app {}", projectDirectory);
-            ProcessBuilder packageApp = new ProcessBuilder("bash", "-c", "mvn package -DskipTests").inheritIO();
-            packageApp.directory(new File(projectDirectory));
-            Process packageAppProcess = packageApp.start();
-            packageAppProcess.waitFor(5, TimeUnit.MINUTES);
-            packageAppProcess.destroy();
-            packageAppProcess.waitFor();
             log.info("[Spotbug] Starting to generate Spotbug report for {}", projectDirectory);
-            ProcessBuilder spotbug = new ProcessBuilder("bash", "-c", "mvn -DskipTests com.github.spotbugs:spotbugs-maven-plugin:spotbugs").inheritIO();
+            ProcessBuilder spotbug = new ProcessBuilder("bash", "-c", "mvn compile -DskipTests com.github.spotbugs:spotbugs-maven-plugin:spotbugs");
             spotbug.directory(new File(projectDirectory));
             Process spotbugProcess = spotbug.start();
             spotbugProcess.waitFor(5, TimeUnit.MINUTES);
             spotbugProcess.destroy();
             spotbugProcess.waitFor();
             log.info("[Spotbug] Report ready to process {}", projectDirectory);
-            SpotbugReportXML spotbugReportXML = processXmlReport(projectDirectory + File.separatorChar + "target" + File.separatorChar + "spotbugsXml.xml");
+            searchForReports(spotbugReportXMLS, projectDirectory);
+
             log.info("[Spotbug] Scan completed");
-            return convertSpotbugReportIntoVulnList(spotbugReportXML);
+            return convertSpotbugReportIntoVulnList(spotbugReportXMLS);
         } catch (Exception e) {
             log.error("[Spotbug] Error occuredd during scanning reason {} on line {}", e.getLocalizedMessage(), e.getStackTrace()[0].getLineNumber());
             return new ArrayList<>();
